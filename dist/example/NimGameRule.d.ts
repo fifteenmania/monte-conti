602acefac9a45647f56881fa76332d8c290efca5
fifteenmania <ktthee1995@gmail.com>
2022-01-05 17:52:51 +0900

    chore: remove dist

--- dist/example/NimGameRule.d.ts
+++
@@ -1,8 +0,0 @@
-import { GameRule } from "../SearchNode";
-export interface NimGameSetting {
-    maxCall: number;
-    numEnd: number;
-    numPeople: number;
-    minCall: number;
-}
-export declare const nimGameRule: GameRule<number>;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NimGameRule_1 = require("./example/NimGameRule");
-const SearchNode_1 = require("./SearchNode");
-const sg = new SearchNode_1.MonteCarloSearchGraph(28, NimGameRule_1.nimGameRule);
-sg.monteCarloSearch(1);
